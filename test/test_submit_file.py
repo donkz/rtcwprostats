e12192f20362d2c96c03b410c530dfecd6209ea6
donkz <szverev@gmail.com>
2021-06-24 11:44:10 -0500

    Testing tools

--- test/test_submit_file.py
+++ test/test_submit_file.py
@@ -6,6 +6,47 @@ import json
 import sys
 import boto3
 import json
+import logging
+
+def get_files(test_dir):
+    test_files = [] 
+    for subdir, dirs, files in os.walk(test_dir):
+            for file in files:
+                #print os.path.join(subdir, file)
+                filepath = subdir + os.sep + file
+                if filepath.endswith(".json"):
+                    test_files.append(filepath)
+    return test_files
+
+def submit_file(file_name):
+    logger.info("Reading from " + os.path.abspath(file_name))
+    logger.info("Test files left " + str(len(test_files)))
+    with open(file_name) as file:
+        content = file.read()
+    
+    match = json.loads(content)
+    match_id_round ="restarted"
+    if '"label": "map_restart"' not in content:
+        match_id_round = match["gameinfo"]["match_id"] + match["gameinfo"]["round"]
+        
+        content_string_bytes = content.encode("ascii") 
+        base64_bytes = base64.b64encode(content_string_bytes) 
+        base64_string = base64_bytes.decode("ascii") 
+        str_len = str(int(len(base64_string)/1024))
+        logger.info(f"Submitting string of {str_len} KB")
+        
+        
+        # =============================================================================
+        # TEST SUBMISSION OF STATS TO WEB API
+        # =============================================================================
+        url = url_api+url_path_submit
+        response = requests.post(url, data=base64_string, headers={'matchid': match_id_round[0:-1], 'x-api-key':'rtcwproapikeythatisjustforbasicauthorization'})
+        logger.info(response.text)
+    else:
+        logger.info("map_restart detected, not submitting")
+    return match_id_round
+
+boto3.set_stream_logger('boto3.resources', logging.INFO)
 
 dynamodb = boto3.resource('dynamodb')
 table = dynamodb.Table("rtcwprostats-database-DDBTable2F2A2F95-1BCIOU7IE3DSE")
@@ -15,43 +56,17 @@ url_path_submit = 'submit'
 test_dir = r".\gamestats5"
 test_dir = r"C:\c\.wolf\rtcwpro\stats\\"
 
-test_files = [] 
-for subdir, dirs, files in os.walk(test_dir):
-        for file in files:
-            #print os.path.join(subdir, file)
-            filepath = subdir + os.sep + file
-            if filepath.endswith(".json"):
-                test_files.append(filepath)
-                
-                
-#file_name = r".\gamestats5\gameStats_match_1610684722_round_2_mp_base.json"
+log_level = logging.INFO
+logging.basicConfig(format='%(name)s:%(levelname)s:%(message)s')
+logger = logging.getLogger("test_submit")
+logger.setLevel(log_level)
+
+
+test_files = get_files(test_dir)
+
 file_name = test_files[0]
 test_files.remove(file_name)
-
-print("Reading from " + os.path.abspath(file_name))
-print("Test files left " + str(len(test_files)))
-with open(file_name) as file:
-    content = file.read()
-
-match = json.loads(content)
-if '"label": "map_restart"' not in content:
-    match_id_round = match["gameinfo"]["match_id"] + match["gameinfo"]["round"]
-    
-    content_string_bytes = content.encode("ascii") 
-    base64_bytes = base64.b64encode(content_string_bytes) 
-    base64_string = base64_bytes.decode("ascii") 
-    str_len = str(int(len(base64_string)/1024))
-    print(f"Submitting string of {str_len} KB")
-    
-    
-    # =============================================================================
-    # TEST SUBMISSION OF STATS TO WEB API
-    # =============================================================================
-    url = url_api+url_path_submit
-    response = requests.post(url, data=base64_string, headers={'matchid': match_id_round[0:-1], 'x-api-key':'rtcwproapikeythatisjustforbasicauthorization'})
-    print(response.text)
-else:
-    print("map_restart detected, no submitting")
+match_id_round = submit_file(file_name)
 
 #curl --location --request POST 'https://rtcwproapi.donkanator.com/submit' 
 #--header 'matchid: 123455678' 
@@ -59,7 +74,7 @@ else:
 #--data-raw 'ewogICAgImdsb3NzYXJ5IjogewogICAgICAgICJ0aXRsZSI6ICJleGFtcGxlIGdsb3NzYXJ5IiwKCQkiR2xvc3NEaXYiOiB7CiAgICAgICAgICAgICJ0aXRsZSI6ICJTIiwKCQkJIkdsb3NzTGlzdCI6IHsKICAgICAgICAgICAgICAgICJHbG9zc0VudHJ5IjogewogICAgICAgICAgICAgICAgICAgICJJRCI6ICJTR01MIiwKCQkJCQkiU29ydEFzIjogIlNHTUwiLAoJCQkJCSJHbG9zc1Rlcm0iOiAiU3RhbmRhcmQgR2VuZXJhbGl6ZWQgTWFya3VwIExhbmd1YWdlIiwKCQkJCQkiQWNyb255bSI6ICJTR01MIiwKCQkJCQkiQWJicmV2IjogIklTTyA4ODc5OjE5ODYiLAoJCQkJCSJHbG9zc0RlZiI6IHsKICAgICAgICAgICAgICAgICAgICAgICAgInBhcmEiOiAiQSBtZXRhLW1hcmt1cCBsYW5ndWFnZSwgdXNlZCB0byBjcmVhdGUgbWFya3VwIGxhbmd1YWdlcyBzdWNoIGFzIERvY0Jvb2suIiwKCQkJCQkJIkdsb3NzU2VlQWxzbyI6IFsiR01MIiwgIlhNTCJdCiAgICAgICAgICAgICAgICAgICAgfSwKCQkJCQkiR2xvc3NTZWUiOiAibWFya3VwIgogICAgICAgICAgICAgICAgfQogICAgICAgICAgICB9CiAgICAgICAgfQogICAgfQp9'
 
 sleep_time = 10 #seconds
-print(f"Sleeping for {sleep_time} seconds")
+logger.info(f"Sleeping for {sleep_time} seconds")
 time.sleep(sleep_time)
 
 # =============================================================================
@@ -72,34 +87,34 @@ time.sleep(sleep_time)
 # ===========
 response = table.get_item(Key={"pk": "match", 'sk': match_id_round}, ReturnConsumedCapacity='TOTAL')
 if "Item" not in response:
-    print("Item not found.")
+    logger.warning("Item not found.")
 else:
     print("Cost of the call: " + str(response['ConsumedCapacity']['CapacityUnits']) + " capacity units")
     data = json.loads(response['Item']["data"])
     match_id = data['match_id']
     round_id = data['round']
     map_name = data['map']
-    print(f"Found match {match_id} round {round_id} on {map_name}")
+    logger.info(f"Found match {match_id} round {round_id} on {map_name}")
 
 # ========
 # statsall
 # ========
 response = table.get_item(Key={"pk": "statsall", 'sk': match_id_round[0:-1]}, ReturnConsumedCapacity='TOTAL')
 if "Item" not in response:
-    print("Item statsall not found.")
+    logger.warning("Item statsall not found.")
 else:
     print("Cost of the call: " + str(response['ConsumedCapacity']['CapacityUnits']) + " capacity units")
     data = json.loads(response['Item']["data"])
     stats_match_id = match_id_round[0:-1]
     num_players = len(data[0].keys()) + len(data[1].keys())
-    print(f"Found statsall for {stats_match_id} with {num_players} players")
+    logger.info(f"Found statsall for {stats_match_id} with {num_players} players")
     
 # ========
 #wstatsall
 # ========
 response = table.get_item(Key={"pk": "wstatsall", 'sk': match_id_round[0:-1]}, ReturnConsumedCapacity='TOTAL')
 if "Item" not in response:
-    print("Item wstatsall not found.")
+    logger.warning("Item wstatsall not found.")
 else:
     print("Cost of the call: " + str(response['ConsumedCapacity']['CapacityUnits']) + " capacity units")
     data = json.loads(response['Item']["data"])
@@ -108,7 +123,7 @@ else:
     num_players = len(data)
     for player in data:
         weapons += len(player[list(player.keys())[0]])
-    print(f"Found wstatsall for match {wstats_match_id} with {num_players} and {weapons} weapons")
+    logger.info(f"Found wstatsall for match {wstats_match_id} with {num_players} and {weapons} weapons")
 
     
 # =============================================================================
@@ -120,4 +135,50 @@ url = url_api + url_path_match
 response = requests.get(url)
 statsall = json.loads(response.text)
 num_players = len(statsall["statsall"][0].keys()) + len(statsall["statsall"][1].keys())
-print(f"Found web statsall for match {stats_match_id} with {num_players}")
+logger.info(f"Found web statsall for match {stats_match_id} with {num_players}")
+
+os.chdir("C:\\Users\\xxx\\Documents\\Github\\rtcwprostats\\test")
+def submit_batch(test_files, size):
+    submitted = 0
+    
+    try:
+        with open('submitted_files.txt','r') as f:
+            lines = f.read().splitlines()
+    except FileNotFoundError:
+        print("File not found, starting fresh.")
+        lines = []
+    submitted_files_dict = dict(zip(lines,"1"*len(lines)))
+    submitted_files = open("submitted_files.txt", "a")
+
+    try: 
+        print("Scanning through " + str(len(test_files)) + " files.")
+        for file_name in test_files:
+            # print("looping through " + file_name)
+            if file_name.split('\\')[-1] in submitted_files_dict:
+                # print("Skipping submitted file.")
+                #test_files.remove(file_name)
+                continue
+            submit_file(file_name)
+            submitted +=1
+            #test_files.remove(file_name)
+            logger.info("Submitted " + str(submitted) + " files out of " + str(size))
+            if submitted >= size:
+                print("Finished " +str(submitted) + "//" + str(size) + " iterations.")
+                break
+            submitted_files.write(file_name.split('\\')[-1]+"\n")
+            time.sleep(2)
+        print("Out of cycle.")
+    except Exception as ex:
+        template = "An exception of type {0} occurred. Arguments:\n{1!r}"
+        error_msg = template.format(type(ex).__name__, ex.args)
+        message = "Failed to load file " + file_name + "\n" + error_msg
+        logger.info(message)
+    finally:
+        submitted_files.close()
+
+    
+    
+    
+    
+        
+    
