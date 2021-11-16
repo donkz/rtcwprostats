56aaeb5fc39a5164ab8d555e88fa7c524e65a0a6
donkz <szverev@gmail.com>
2021-11-16 15:48:02 -0600

    Gamelog community template

---
+++ test/gamelog_process/longest_kill.py
@@ -0,0 +1,67 @@
+import math
+from award_class import AwardClass
+
+"""
+relevant json element
+{
+    match_id: "1630953060",
+    round_id: "1",
+    unixtime: "1630953652",
+    group: "player",
+    label: "kill",
+    agent: "68deaefc0a07be79fcb2cc5104a71b1e",
+    other: "5379320f3c64f43cdaf3350fc13011ce",
+    weapon: "MP-40",
+    other_health: 125,
+    agent_pos: "589.451965,-70.798233,69.000000",
+    agent_angle: "79.161987",
+    other_pos: "2791.199219,4.931318,188.575974",
+    other_angle: "-88.071899",
+    allies_alive: "5",
+    axis_alive: "1"
+}
+"""
+
+
+class LongestKill(AwardClass):
+    """Calculate the longest kill in the game."""
+
+    award_name = "Longest Kill"
+
+    def __init__(self):
+        super().__init__(self.award_name)
+
+    def process_event(self, rtcw_event):
+        """Take incoming kill with SMG or Pistol and see if it's a longest one for the killer."""
+        try:
+            if rtcw_event.get("label", None) == "kill" and rtcw_event.get("weapon", None) in ["MP-40", "Thompson", "Sten", "Colt", "Luger"]:
+                killer = rtcw_event.get("agent", "no guid")
+                x1, x2, y1, y2, z1, z2 = self.get_coordinates(rtcw_event)
+                dist = int(math.sqrt((x2 - x1)**2 + (y2 - y1)**2 + (z2 - z1)**2))
+                if self.players_values.get(killer, 0) < dist:
+                    self.players_values[killer] = dist
+        except Exception as ex:
+            template = "An exception of type {0} occurred. Arguments:\n{1!r}"
+            error_msg = template.format(type(ex).__name__, ex.args)
+            if self.debug:
+                print("error at " + self.__name__ + " process event\n")
+                print(error_msg)
+
+    def get_coordinates(self, rtcw_event):
+        """Extract coorinates from the position string."""
+        x1 = x2 = y1 = y2 = z1 = z2 = 0
+        try:
+            agent_coord = rtcw_event["agent_pos"].split(",")
+            other_coord = rtcw_event["other_pos"].split(",")
+            x1 = float(agent_coord[0])
+            y1 = float(agent_coord[1])
+            z1 = float(agent_coord[2])
+            x2 = float(other_coord[0])
+            y2 = float(other_coord[1])
+            z2 = float(other_coord[2])
+        except Exception as ex:
+            if self.debug:
+                print("error at " + rtcw_event.get("unixtime", "no_unixtime") + ": could not split coordinates." + ex.args)
+            # exception handling and reporting is not desired here because of data volumes and who receives them
+
+        return x1, x2, y1, y2, z1, z2
--- test/gamelog_process/main.py
+++ test/gamelog_process/main.py
@@ -10,26 +10,55 @@ logger.setLevel(log_level)
 
 url_api = 'https://rtcwproapi.donkanator.com/'
 api_path = "gamelogs/"
-match_round_id = "16309530601" #get more from https://rtcwproapi.donkanator.com/matches/type/na/6. Use match_round_id value
-url = url_api + api_path + match_round_id
+match_id = "1630953060"  # get more from https://rtcwproapi.donkanator.com/matches/type/na/6. Use match_round_id value
+round_id = "1"
+url = url_api + api_path + match_id + round_id
 
-logger.info("Reading gamelog from match {match} round {round_id}".format(match = match_round_id[0:-1], round_id = match_round_id[-1:]))
+logger.info("Reading gamelog from match {match_id} round {round_id}".format(match_id=match_id, round_id=round_id))
 response = requests.get(url)
-gamelog = json.loads(response.text)
+gamelog_r1 = json.loads(response.text)
+
+round_id = "2"
+url = url_api + api_path + match_id + round_id
+
+logger.info("Reading gamelog from match {match_id} round {round_id}".format(match_id=match_id, round_id=round_id))
+response = requests.get(url)
+gamelog_r2 = json.loads(response.text)
+
+gamelog = []
+gamelog.extend(gamelog_r1)
+gamelog.extend(gamelog_r2)
+# there could be more than 2 or just 1 rounds here
+
 
 # everything above this line is just a test setup
+# you now have 2 rounds of one match
+# it is a list ordered in the same way RTCWPro produced it
 # -----------------------------------------------
 # below is your code
-# please put the definitions into its own module
-# and expose just the main function to get a dictionary in the form of {"award_name": {results}}
+# please create your own class that once instantiated takes events line by line and
+# produces a dictionary in the form of {"award_name": {"guid":"1234", "value":"15"}}
 # please use native python and packages that come by default
 # for full list refer here: https://insidelambda.com/lambda-python38.txt
 
 
-from mymodule import my_function
-my_result_dict = my_function(gamelog)
-print(my_result_dict)
+from longest_kill import LongestKill
+from frontliner import Frontliner
+# add your class here
 
+longest_kill = LongestKill()
+frontliner = Frontliner()
+# init your class here
 
+# Loop through all events in the match or a group and feed events to different award calculator classes
+for rtcw_event in gamelog:
+    longest_kill.process_event(rtcw_event)
+    frontliner.process_event(rtcw_event)
+    # your_class.process_event(rtcw_event)
 
+awards = {}
+awards.update(longest_kill.get_all_top_results())
+awards.update(frontliner.get_all_top_results())
+# insert your result here
 
+print(awards)
